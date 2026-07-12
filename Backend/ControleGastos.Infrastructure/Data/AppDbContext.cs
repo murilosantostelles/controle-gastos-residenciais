b59322fb68ebd548af9d62929ced4d203d2b1771
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-07-12 11:07:13 -0300

    feat: implementa AppDbContext com regra de cascade delete
    configura mapeamento de Pessoa e Transacao, alem da deleção em cascata das transações ao remover uma pessoa

---
+++ Backend/ControleGastos.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+using ControleGastos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Pessoa> Pessoas { get; set; }
+    public DbSet<Transacao> Transacoes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // faz com que, ao deletar uma pessoa, todas as transações associadas a ela também sejam deletadas
+        modelBuilder.Entity<Transacao>()
+            .HasOne(t => t.Pessoa)
+            .WithMany(p => p.Transacoes)
+            .HasForeignKey(t => t.PessoaId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
